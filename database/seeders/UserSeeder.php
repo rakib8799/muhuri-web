ff4214a0f1e4cc548a32e028995030cc74653622
rakib8799 <mkrakib007@gmail.com>
2025-05-15 11:08:27 +0600

    Update .env.example, deploy.sh and UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -18,12 +19,17 @@ class UserSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('users')->truncate();
-        $user = User::factory()->create([
+
+        $user = User::create([
             'name' => Constants::ROLE_ADMIN,
             'email' => '01332995033' . '@' . Constants::SUB_DOMAIN . '.' . Constants::DOMAIN,
+            'email_verified_at' => now(),
             'mobile_number' => '01332995033',
-            'password' => Hash::make('12345'),
+            'mobile_number_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10)
         ]);
+
         $user->assignRole(Constants::ROLE_SUPER_ADMIN);
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ exec 2>&1
 echo "🚀 Starting full Laravel + Inertia + Vue.js deployment..."
 
 # === CONFIGURATION ===
-USER="muhuri"
+USER="admin"
 SUB_DOMAIN="muhuri-web"
 DOMAIN="mkrdev.xyz"
 APP_DIR="/home/$USER/web/$SUB_DOMAIN.$DOMAIN/public_html"
